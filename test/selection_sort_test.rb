1a009c19bdd0beb3c2f7bfc4df1ec610b58e98c5
Duyet Nguyen <duyetln@yahoo.com>
2013-04-14 15:06:47 -0700

    Added unit test for selection sort

---
+++ test/selection_sort_test.rb
@@ -0,0 +1,33 @@
+require '../source/selection_sort.rb'
+require 'test/unit'
+
+class SelectionSortTest < Test::Unit::TestCase
+
+  def setup
+    @empty = []
+    @one = [2]
+    @two = [4, 3]
+    @many = [1, 5, 2, 6, 3, 7, 9, 2]
+    @identical = [3, 3, 3, 2, 2, 1, 1, 6]
+    
+    @empty_orig = @empty.dup
+    @one_orig = @one.dup
+    @two_orig = @two.dup
+    @many_orig = @many.dup
+    @identical_orig = @identical.dup
+  end
+  
+  def test_selection_sort
+    assert selection_sort(@empty) == @empty.sort
+    assert selection_sort(@one) == @one.sort
+    assert selection_sort(@two) == @two.sort
+    assert selection_sort(@many) == @many.sort
+    assert selection_sort(@identical) == @identical.sort
+    
+    assert @empty == @empty_orig
+    assert @one == @one_orig
+    assert @two == @two_orig
+    assert @many == @many_orig
+    assert @identical == @identical_orig
+  end
+end
