904ca0561d3fffe16f6d7220aba6d5810d317cae
Duyet Nguyen <duyetln@yahoo.com>
2013-04-14 15:05:52 -0700

    Renamed some variables in bubble sort implementation

--- source/bubble_sort.rb
+++ source/bubble_sort.rb
@@ -2,15 +2,15 @@ def bubble_sort(array)
   result = array.dup
   return result if result.length <= 1
   
-  last_unsorted = result.length
+  boundary_index = result.length
   
-  while last_unsorted > 0
+  while boundary_index > 0
     result.each_index do |index|
-      if index < last_unsorted - 1 && result[index] > result[index+1]
-        result[index], result[index+1] = result[index+1], result[index] 
+      if index < boundary_index - 1 && result[index + 1] < result[index]
+        result[index], result[index + 1] = result[index + 1], result[index] 
       end
     end
-    last_unsorted -= 1
+    boundary_index -= 1
   end
   result
 end
