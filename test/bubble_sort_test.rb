9f260abfa100908a932cdb69575b1d4fa1c4b935
Duyet Nguyen <duyetln@yahoo.com>
2013-04-14 14:28:41 -0700

    Added unit test for bubble sort

---
+++ test/bubble_sort_test.rb
@@ -0,0 +1,33 @@
+require '../source/bubble_sort.rb'
+require 'test/unit'
+
+class BubbleSortTest < Test::Unit::TestCase
+
+  def setup
+    @empty = []
+    @one = [2]
+    @two = [4, 3]
+    @many = [1, 5, 2, 6, 3, 7, 9, 2]
+    @identical = [3, 3, 3, 2, 2, 1, 1, 6]
+    
+    @empty_orig = @empty.dup
+    @one_orig = @one.dup
+    @two_orig = @two.dup
+    @many_orig = @many.dup
+    @identical_orig = @identical.dup
+  end
+  
+  def test_bubble_sort
+    assert bubble_sort(@empty) == @empty.sort
+    assert bubble_sort(@one) == @one.sort
+    assert bubble_sort(@two) == @two.sort
+    assert bubble_sort(@many) == @many.sort
+    assert bubble_sort(@identical) == @identical.sort
+    
+    assert @empty == @empty_orig
+    assert @one == @one_orig
+    assert @two == @two_orig
+    assert @many == @many_orig
+    assert @identical == @identical_orig
+  end
+end
